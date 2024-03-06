7c6351bdb1e687509cfd02ffb6000b5214debada
Jon <Jonzheng137@gmail.com>
2024-03-06 13:33:14 -0800

    Initial Commit

---
+++ LyftprojtSwiftUI/LyftprojtSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyftprojtSwiftUIApp.swift
+//  LyftprojtSwiftUI
+//
+//  Created by Jonathan Zheng on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LyftprojtSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
