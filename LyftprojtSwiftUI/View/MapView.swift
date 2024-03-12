de1a89d412dfc977128e15e6601cb6473ad365d6
Jonathan <jonzheng137@gmail.com>
2024-03-12 14:25:08 -0700

    Created MapViewUI

--- LyftprojtSwiftUI/View/MapView.swift
+++ LyftprojtSwiftUI/View/MapView.swift
@@ -4,28 +4,40 @@
 //
 //  Created by Jonathan Zheng on 3/6/24.
 //
-
+import CoreLocation
 import SwiftUI
 import MapKit
+
+struct AnnotationView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
 
 struct MapView: View {
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.787359, longitude: -122.408227), // Default to San Francisco
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
+    @State private var annotations = [MapAnnotation<AnnotationView>]()
     
-
-    //let mapViewModel = mapViewViewModel
+    //decided to put here
     var body: some View {
         GeometryReader { geometry in
             VStack(alignment: .leading, spacing: 0) {
-                Map(coordinateRegion: $region, showsUserLocation: true)
-                            .onAppear {
-                                // Additional setup or customization can be done here
-                                region.center = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
-                                region.span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-                            } // add stuff here later for on appear
+                Map(initialPosition: MapCameraPosition.region(region), content: {
+                    Marker("Hello", coordinate: region.center)
+                })
+                // request for authorization then
+                
+                // add stuff here later for on appear
+                Text("Rise and Shine")
+                    .padding()
+                Text("Where are you Going?")
+                    .bold()
+                    .padding()
                 HStack{
+                    
                     Image("search")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
@@ -35,6 +47,8 @@ struct MapView: View {
                     Button("Search Destination") {
                         
                     }
+                    
+                    .frame(maxWidth: .infinity)
                     .background(Color.white)
                     .foregroundColor(.gray)
                     .cornerRadius(5)
@@ -43,6 +57,7 @@ struct MapView: View {
                             .stroke(Color.gray, lineWidth: 2)
                     )
                 }
+                .padding(.trailing, 10)
                 
                 HStack{
                     Image("location-custom")
@@ -59,13 +74,9 @@ struct MapView: View {
                             .font(.caption.italic())
                     }
                 }
-                }
-            
+            }
             .frame(height: geometry.size.height)
         }
     }
 }
-
-#Preview {
-    MapView()
-}
+//
